ba9bbfc34eb141910a47b372e77caa192d9aafea
ux-button <ak@AKs-MacBook-Pro.local>
2024-11-24 16:42:51 +0300

    Fix: change origin to netlify

--- back/config/corsConfig.js
+++ back/config/corsConfig.js
@@ -1,5 +1,6 @@
 const corsOptions = {
-  origin: "http://localhost:5173",
+  // origin: "http://localhost:5173",
+  origin: "https://67432c65969e40a4502ea37c--selfdrive-looty.netlify.app/",
   credentials: true,
 };
 
